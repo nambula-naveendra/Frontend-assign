3a8794b65281daf159646d406194e0b564ff3d1d
Naveendra Nambula <naveendranambula@gmail.com>
2019-04-27 14:56:32 +0530

    Update new.js

--- new.js
+++ new.js
@@ -1,10 +1,24 @@
-document.querySelector('.dropdown-item').addEventListener('click',function(){
+
+document.getElementById('#dropdown-item-1').addEventListener('click',function(){
+  var select = document.querySelector('.dropdown-item').textContent; 
+ 
+  if(select === "Rupees")
+  {
+    document.querySelector('.required').textContent= (67.461643)*document.querySelector('.required').textContent;
+    console.log("hello world");
+  }
+
+});
+
+document.getElementById('#dropdown-item-2').addEventListener('click',function(){
   var select = document.querySelector('.dropdown-item').textContent;
 
   if(select === "USD")
   {
     
-    document.querySelector('.required').textContent= (0.75)*
+    document.querySelector('.required').textContent= (0.0142823238)*document.querySelector('.required').textContent;
+    console.log("helo");
   }
+  
 
 });
