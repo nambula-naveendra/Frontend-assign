ba88ab702c4f78e0c6861222c2a684787b7eaf49
Naveendra Nambula <naveendranambula@gmail.com>
2019-04-27 13:58:08 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,10 @@
+document.querySelector('.dropdown-item').addEventListener('click',function(){
+  var select = document.querySelector('.dropdown-item').textContent;
+
+  if(select === "USD")
+  {
+    
+    document.querySelector('.required').textContent= (0.75)*
+  }
+
+});
